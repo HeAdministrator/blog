6a4ca50ddabbb6d41c003a16fc7a6f01d77aa913
allen <1351242029@qq.com>
2019-06-14 01:15:12 +0800

    二次提交

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,24 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
 });
+
+Route::get("send_mail",function(){
+    mail('1351242029@qq.com',"测试邮件","这是一封测试邮件");
+    return 'send mail';
+});
+
+
+Route::post('user/', function ($user_name,$password) {
+    if($password=="123456"){
+        return "欢迎您登录: " . $user_name."!!!";
+    }else{
+        return "密码输入不正确！";
+    }
+
+})->name('user.login');
+
+Route::get('user/{id?}', function ($id = 1) {
+    return "用户ID: " . $id;
+})->name('user.profile');
